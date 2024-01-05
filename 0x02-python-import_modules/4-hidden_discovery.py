35d4fa1b3f9da4722cef9114c0967f04f58249a0
touil-abdelilah <133289630+touil-abdelilah@users.noreply.github.com>
2024-01-05 02:23:43 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-import dis
 import types
+import sys
 
-def print_names(module):
-    bytecode = module.__code__.co_code
-    instructions = dis.get_instructions(bytecode)
-    names = set()
-    for instruction in instructions:
-        if instruction.opname == 'LOAD_CONST':
-            const = module.__code__.co_consts[instruction.arg]
-            if isinstance(const, str) and not const.startswith('__'):
-                names.add(const)
+def extract_names(file_path):
+    with open(file_path, 'rb') as file:
+        magic_number = file.read(4)
+        mod_time = file.read(4)
+        code_obj = types.CodeType.from_traceback(file.read())
 
-    for name in sorted(names):
-        print(name)
+        names = sorted(set(code_obj.co_names))
+        for name in names:
+            if not name.startswith('__'):
+                print(name)
 
 if __name__ == "__main__":
-    with open('hidden_4.pyc', 'rb') as file:
-        code = file.read()
-        module = types.CodeType(
-            0, 0, 0, 0, 67, code, (), (), (), '', '', 0, b'')
-        print_names(types.ModuleType(module))
+    file_path = 'hidden_4.pyc'
+    extract_names(file_path)
