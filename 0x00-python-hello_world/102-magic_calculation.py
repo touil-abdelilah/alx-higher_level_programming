0713f5ba4d87db997ead1a3df82e953315f3d2db
touil-abdelilah <et.touil505@gmail.com>
2024-01-04 16:22:34 -0800

    Update

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 98  # LOAD_CONST 1 (98)
-    result += a  # LOAD_FAST a, BINARY_ADD
-    result **= b  # LOAD_FAST b, BINARY_POWER
-    return result
+    result = 98                # LOAD_CONST 1 (98)
+    result = (result ** a)     # LOAD_FAST a, BINARY_POWER
+    result += b                # LOAD_FAST b, BINARY_ADD
+    return result              # RETURN_VALUE
+magic_calculation()
