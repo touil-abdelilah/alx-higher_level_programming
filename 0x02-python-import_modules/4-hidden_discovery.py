ae52efaa41f8a49ff5de611fd24ea749d14b3aa0
touil-abdelilah <et.touil505@gmail.com>
2024-01-04 17:38:00 -0800

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+import marshal
 import types
-import sys
 
 def extract_names(file_path):
     with open(file_path, 'rb') as file:
         magic_number = file.read(4)
         mod_time = file.read(4)
-        code_obj = types.CodeType.from_traceback(file.read())
+        code_obj = marshal.load(file)
 
         names = sorted(set(code_obj.co_names))
         for name in names:
