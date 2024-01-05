00f9acda43688940e231904be502b18d6173565b
touil-abdelilah <et.touil505@gmail.com>
2024-01-04 17:34:19 -0800

    3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,11 +3,6 @@ import sys
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    num_args = len(args)
+    result = sum(int(arg) for arg in args)
+    print(result)
 
-    if num_args == 0:
-        print("0 arguments.")
-    else:
-        print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
-        for i, arg in enumerate(args, 1):
-            print("{}: {}".format(i, arg))
