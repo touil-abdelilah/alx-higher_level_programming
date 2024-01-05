50fd9e9030a8c380d9453fdccd24e0377a60732c
touil-abdelilah <133289630+touil-abdelilah@users.noreply.github.com>
2024-01-05 02:09:04 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,5 @@ import sys
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    num_args = len(args)
-
-    if num_args == 0:
-        print("0 arguments.")
-    else:
-        print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
-        for i, arg in enumerate(args, 1):
-            print("{}: {}".format(i, arg))
+    total = sum(map(int, args))
+    print(total)
