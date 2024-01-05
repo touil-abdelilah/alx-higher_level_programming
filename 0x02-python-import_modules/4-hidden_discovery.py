b446f392aa2fdb3b0726e849427a5302a2f49288
touil-abdelilah <133289630+touil-abdelilah@users.noreply.github.com>
2024-01-05 02:21:21 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-import marshal
+import dis
+import types
+
+def print_names(module):
+    bytecode = module.__code__.co_code
+    instructions = dis.get_instructions(bytecode)
+    names = set()
+    for instruction in instructions:
+        if instruction.opname == 'LOAD_CONST':
+            const = module.__code__.co_consts[instruction.arg]
+            if isinstance(const, str) and not const.startswith('__'):
+                names.add(const)
+
+    for name in sorted(names):
+        print(name)
 
 if __name__ == "__main__":
     with open('hidden_4.pyc', 'rb') as file:
-        magic = file.read(4)
-        mod_date = file.read(4)
-        code_obj = marshal.load(file)
-
-        names = [name for name in code_obj.co_names if not name.startswith('__')]
-        names.sort()
-
-        for name in names:
-            print(name)
+        code = file.read()
+        module = types.CodeType(
+            0, 0, 0, 0, 67, code, (), (), (), '', '', 0, b'')
+        print_names(types.ModuleType(module))
