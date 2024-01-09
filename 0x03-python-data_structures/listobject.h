e9f34e81da548639c14a6110155d8327f6777897
et.touil <et.touil505@gmail.com>
2024-01-09 02:30:19 +0100

    Create a C function that prints some basic info about Python lists.

---
+++ 0x03-python-data_structures/listobject.h
@@ -0,0 +1,9 @@
+#ifndef _PYTHON_LISTOBJECT_H_
+#define _PYTHON_LISTOBJECT_H_
+
+#include <Python.h>
+
+/* Structure declarations or any required function prototypes */
+
+#endif /* _PYTHON_LISTOBJECT_H_ */
+
---
+++ 0x03-python-data_structures/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "listobject.h"
+
+void print_python_list_info(PyObject *p) {
+    /* Implementation of the print_python_list_info function */
+}
+
