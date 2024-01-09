ae438fba03a0291b4813f4127127c0a804ccd905
et.touil <et.touil505@gmail.com>
2024-01-09 02:34:40 +0100

    Create a C function that prints some basic info about Python lists.

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,8 +1,10 @@
+#include <Python.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include "listobject.h"
 
 void print_python_list_info(PyObject *p) {
-    /* Implementation of the print_python_list_info function */
+    // Your implementation goes here
+    // Extract information about the Python list and print it
+    // Use Python C API functions to achieve this
 }
 
+// Rest of your code...
