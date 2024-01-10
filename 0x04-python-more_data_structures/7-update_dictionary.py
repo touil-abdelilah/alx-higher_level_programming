8668e0947022a530c0ea119b5d441032e5103e29
et.touil <et.touil505@gmail.com>
2024-01-10 18:59:20 +0100

    Write a function that replaces or adds key/value in a dictionary.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+
+# Function to print the dictionary by ordered keys
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary.keys()):
+        print("{}: {}".format(key, a_dictionary[key]))
