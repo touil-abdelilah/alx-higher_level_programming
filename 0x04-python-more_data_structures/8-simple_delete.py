b7b0a56368ef8eb0058731421ac314cb2507c0d3
et.touil <et.touil505@gmail.com>
2024-01-10 19:24:05 +0100

    Write a function that deletes a key in a dictionary.

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    try:
+    # Check if the key exists before deleting
+    if key in a_dictionary:
         del a_dictionary[key]
-    except KeyError:
-        pass
-
     return a_dictionary
-
-def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
-        print("{}: {}".format(key, a_dictionary[key]))
