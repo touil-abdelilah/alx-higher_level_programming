5269d352836a0065b2603986af6cee4bf1f746a5
et.touil <et.touil505@gmail.com>
2024-01-10 20:03:54 +0100

    check3

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,53 +1,55 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-void print_python_bytes(PyObject *p) {
-    printf("[.] bytes object info\n");
-    if (PyBytes_Check(p)) {
-        Py_ssize_t size = PyBytes_Size(p);
-        printf("  size: %ld\n", size);
+/**
+ * print_python_list - prints info about Python lists
+ * @p: PyObject
+ */
+void print_python_list(PyObject *p)
+{
+    Py_ssize_t i, size;
 
-        // Attempt to convert to string
-        PyObject *string_repr = PyObject_Repr(p);
-        const char *string_value = PyUnicode_AsUTF8(string_repr);
-        printf("  trying string: %s\n", string_value);
-
-        // Print up to 10 bytes
-        printf("  first %ld bytes: ", (size < 10) ? size : 10);
-        for (Py_ssize_t i = 0; i < ((size < 10) ? size : 10); ++i) {
-            printf("%02x ", (unsigned char)PyBytes_AS_STRING(p)[i]);
-        }
-        printf("\n");
-        Py_XDECREF(string_repr);
-    } else {
-        printf("  [ERROR] Invalid Bytes Object\n");
-    }
-}
-
-void print_python_list(PyObject *p) {
-    Py_ssize_t size = PyList_Size(p);
     printf("[*] Python list info\n");
+    size = PyList_Size(p);
     printf("[*] Size of the Python List = %ld\n", size);
     printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-    
-    for (Py_ssize_t i = 0; i < size; ++i) {
-        PyObject *element = PyList_GetItem(p, i);
-        printf("Element %ld: ", i);
 
-        if (PyBytes_Check(element)) {
-            print_python_bytes(element);
-        } else if (PyLong_Check(element)) {
-            printf("int\n");
-        } else if (PyFloat_Check(element)) {
-            printf("float\n");
-        } else if (PyTuple_Check(element)) {
-            printf("tuple\n");
-        } else if (PyList_Check(element)) {
-            printf("list\n");
-        } else if (PyUnicode_Check(element)) {
-            printf("str\n");
-        } else {
-            printf("Unknown Type\n");
-        }
+    for (i = 0; i < size; i++)
+    {
+        printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+        if (strcmp(Py_TYPE(PyList_GetItem(p, i))->tp_name, "bytes") == 0)
+            print_python_bytes(PyList_GetItem(p, i));
     }
 }
 
+/**
+ * print_python_bytes - prints info about Python bytes objects
+ * @p: PyObject
+ */
+void print_python_bytes(PyObject *p)
+{
+    Py_ssize_t i, size;
+    char *str;
+
+    printf("[.] bytes object info\n");
+
+    if (!PyBytes_Check(p))
+    {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
+    size = PyBytes_Size(p);
+    printf("  size: %ld\n", size);
+
+    str = PyBytes_AsString(p);
+    printf("  trying string: %s\n", str);
+
+    printf("  first 10 bytes: ");
+    for (i = 0; i < size && i < 10; i++)
+        printf("%02x ", str[i] & 0xff);
+
+    for (; i < 10; i++)
+        printf("00 ");
+    printf("\n");
+}
