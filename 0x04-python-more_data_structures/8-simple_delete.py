8d3c82b6ffd5fd07705b4a367723c05b5ca4a84a
et.touil <et.touil505@gmail.com>
2024-01-10 19:04:21 +0100

    check3

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    # Use try-except to handle the case where the key doesn't exist
     try:
         del a_dictionary[key]
     except KeyError:
@@ -9,7 +8,6 @@ def simple_delete(a_dictionary, key=""):
 
     return a_dictionary
 
-# Function to print the dictionary by ordered keys
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary.keys()):
         print("{}: {}".format(key, a_dictionary[key]))
