c3b393f585efc65e6eaa1b538a9c917f7be1d303
et.touil <et.touil505@gmail.com>
2024-01-10 19:11:08 +0100

    Write a function that returns a new dictionary with all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for key, value in a_dictionary.items():
@@ -9,3 +10,11 @@ def multiply_by_2(a_dictionary):
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary.keys()):
         print("{}: {}".format(key, a_dictionary[key]))
+
+# Test the function
+if __name__ == "__main__":
+    a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+    new_dict = multiply_by_2(a_dictionary)
+    print_sorted_dictionary(a_dictionary)
+    print("--")
+    print_sorted_dictionary(new_dict)
