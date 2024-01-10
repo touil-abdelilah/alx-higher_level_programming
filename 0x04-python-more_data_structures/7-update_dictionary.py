f753e4c17d35a2880e764092fcb00c937e583781
et.touil <et.touil505@gmail.com>
2024-01-10 19:25:30 +0100

    Write a function that deletes a key in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+
 def update_dictionary(a_dictionary, key, value):
+    # Update or add the key-value pair
     a_dictionary[key] = value
     return a_dictionary
-
-# Function to print the dictionary by ordered keys
-def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
-        print("{}: {}".format(key, a_dictionary[key]))
