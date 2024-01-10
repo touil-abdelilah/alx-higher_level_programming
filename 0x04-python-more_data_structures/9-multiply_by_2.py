16ca6ab6577854ccadebc613e9e26122f7cccd34
et.touil <et.touil505@gmail.com>
2024-01-10 19:21:53 +0100

    check3

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for key, value in a_dictionary.items():
         new_dict[key] = value * 2
     return new_dict
-def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
-        print("{}: {}".format(key, a_dictionary[key]))
