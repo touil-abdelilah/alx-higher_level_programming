4e2938e9d036575c7298bd97858b3e89c42b4f8f
et.touil <et.touil505@gmail.com>
2024-01-10 18:15:42 +0100

    1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,5 @@ def search_replace(my_list, search, replace):
     # Create a new list by iterating through the elements of the original list
     # Replace 'search' with 'replace' wherever it occurs
     new_list = [replace if element == search else element for element in my_list]
-
     return new_list
+
