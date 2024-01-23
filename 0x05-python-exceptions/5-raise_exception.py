5d6d3a077586c1cdb3b3677ec73f95474a821f10
et.touil <et.touil505@gmail.com>
2024-01-23 02:53:15 +0100

    Task 5

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+
 def raise_exception():
     raise TypeError("Exception raised")
+
 
 if __name__ == "__main__":
     try:
         raise_exception()
-    except TypeError as te:
-        print(te)
-
+    except TypeError as e:
+        print(e)
