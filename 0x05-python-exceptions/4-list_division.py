a8a0d27a2772410b9ff7d5d7da51baa3b666d7f4
et.touil <et.touil505@gmail.com>
2024-01-23 02:35:56 +0100

    Write a function that divides element by element 2 lists. Prototype: def list_division(my_list_1, my_list_2, list_length):

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,22 +5,15 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             dividend = my_list_1[i]
             divisor = my_list_2[i]
-            division_result = 0
-            # Check if both elements are numbers
-            if not isinstance(dividend, (int, float)):
-                if isinstance(divisor, (int, float)):
-                    raise TypeError("wrong type")
-            # Perform division if divisor is not zero
-            if divisor != 0:
-                division_result = dividend / divisor
-            else:
-                raise ZeroDivisionError("division by 0")
+            if not (isinstance(dividend, (int, float)) and isinstance(divisor, (int, float))):
+                raise TypeError("wrong type")
+            division_result = dividend / divisor if divisor != 0 else 0
         except IndexError:
             print("out of range")
         except TypeError:
             print("wrong type")
-        except ZeroDivisionError as e:
-            print(e)
+        except ZeroDivisionError:
+            print("division by 0")
         finally:
             result.append(division_result)
     return result
