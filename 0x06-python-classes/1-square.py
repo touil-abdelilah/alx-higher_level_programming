91c24dca60d275c3c3e5c542bc12fcf21eb3facd
et.touil <et.touil505@gmail.com>
2024-01-25 00:06:23 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Class that defines a square by its size."""
+
+    def __init__(self, size):
+        """Initialization method with size."""
+        self.__size = size
