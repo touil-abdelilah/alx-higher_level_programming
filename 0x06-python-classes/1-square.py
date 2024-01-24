388adea51b6541d3d587f317079e793105588e9c
et.touil <et.touil505@gmail.com>
2024-01-25 00:19:15 +0100

    Task solved

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+"""int: Module level variable documented inline.
 
+The docstring may span multiple lines. The type may optionally be specified
+on the first line, separated by a colon.
+"""
 class Square:
     """Class that defines a square by its size."""
 
