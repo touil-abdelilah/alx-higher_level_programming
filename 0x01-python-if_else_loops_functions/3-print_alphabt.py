d4e458bd8046b95d65762f022680225f770cc615
et.touil <et.touil505@gmail.com>
2024-01-25 16:38:13 +0100

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(ord('a'), ord('z') + 1):
-    
-    if chr(i) in ['q', 'e']: 
+    if chr(i) in ['q', 'e']:
         pass
     else:
         print("{}".format(chr(i)), end='')
