e0cdface867fb7c5f057f5d60d54e9625bd36d20
et.touil <et.touil505@gmail.com>
2024-02-19 16:36:20 +0100

    Write a function that prints a string in uppercase followed by a new line.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,21 +3,21 @@
 
 def uppercase(s):
     """
-    print a string in uppercase fllowed by a new line.
+    Print a string in uppercase followed by a new line.
 
-   Args:
-        str: The iput string.
+    Args:
+        s: The input string.
 
     Returns:
         None
     """
     for char in s:
-        # convert lowercase letters to uppsercase
+        # Convert lowercase letters to uppercase
         if ord('a') <= ord(char) <= ord('z'):
-            uppercase = chr(ord(char) - (ord('a') - ord('A')))
+            uppercase_char = chr(ord(char) - (ord('a') - ord('A')))
         else:
-            uppercase = char
-        # print each character without newline
-        print("{}".format(uppercase), end="")
-    # print newline
-    print("\n")
+            uppercase_char = char
+        # Print each character without newline
+        print("{}".format(uppercase_char), end="")
+    # Print newline after printing the entire string in uppercase
+    print("")
