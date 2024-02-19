2befd55a2e8247c5db47131990684fd3966afd98
et.touil <et.touil505@gmail.com>
2024-02-19 18:24:37 +0100

    add tow intergers and return the result

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a function that adds tow numbers and return the result
+Args:
+    a:input 1
+    b:input 3
+function:
+    add:addes tow numbers
+"""
+
+
+def add(a, b):
+    return a + b
