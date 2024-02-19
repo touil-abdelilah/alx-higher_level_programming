9fa300d78b971142f416ca83bf04fe309dde7824
et.touil <et.touil505@gmail.com>
2024-02-19 16:49:16 +0100

    fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def uppercase(s):
     """
     Print a string in uppercase followed by a new line.
@@ -11,12 +10,15 @@ def uppercase(s):
     Returns:
         None
     """
+    # Initialize an empty string to store the uppercase characters
+    result = ""
     for char in s:
         # Convert lowercase letters to uppercase
         if ord('a') <= ord(char) <= ord('z'):
             uppercase_char = chr(ord(char) - (ord('a') - ord('A')))
         else:
             uppercase_char = char
-        # Print each character without newline
-        print("{}".format(uppercase_char), end="")
-    # Print newline after printing the entire string in uppercase
+        # Append each uppercase character to the result string
+        result += uppercase_char
+    # Print the entire string containing the uppercase characters
+    print("{}".format(result), end="")
