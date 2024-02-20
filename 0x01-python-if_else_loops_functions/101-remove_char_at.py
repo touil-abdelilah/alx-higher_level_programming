9b46d1650dee064482b55d2513352f448e2b7ecf
et.touil <et.touil505@gmail.com>
2024-02-20 10:36:58 +0100

    Write a function that creates a copy of the string, removing the character at the position n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """
- a function that creates a copy of the string,
- removing the character at the position n
- Args:
-    str: the source string
-    n:the removed character position
-Functions:
-    remove_char_at(str,n):crate a copy of the string str
-    and remove the character at n
+A function that creates a copy of the string,
+removing the character at the position n.
+Args:
+    source_str: The source string.
+    position: The position of the character to remove.
+Returns:
+    The modified string with the character removed.
 """
 
 
 def remove_char_at(str, n):
+    """
+    Create a copy of the string source_str
+    and remove the character at the position position.
+    """
     str_copy = ""
     for i in range(len(str)):
-        if i == n:
+        if i == position:
             continue
         else:
             str_copy += str[i]
-    return(str_copy)
+    return str_copy
