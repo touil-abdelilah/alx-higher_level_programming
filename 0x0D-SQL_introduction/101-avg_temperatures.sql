f9f3997a8123432500fa32c7cc732f58dfc1819b
et.touil <et.touil505@gmail.com>
2024-03-14 19:49:36 +0000

    a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,9 @@
+-- Import the provided table dump into the hbtn_0c_0 database
+-- (Assuming the file name is "table_dump.sql")
+-- mysql -hlocalhost -uroot -p hbtn_0c_0 < table_dump.sql
+
+-- Script to calculate the average temperature (Fahrenheit) by city ordered by temperature (descending)
+SELECT city, AVG(temperature) AS avg_temp
+FROM temperature_data
+GROUP BY city
+ORDER BY avg_temp DESC;
