26d5e8d3fa071a37581ec2d5581a4cd0f1ab5225
et.touil <et.touil505@gmail.com>
2024-03-14 20:21:16 +0000

    Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- 0-privileges.sql
+
+-- List all privileges of the MySQL users user_0d_1 and user_0d_2 on the server (localhost).
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
