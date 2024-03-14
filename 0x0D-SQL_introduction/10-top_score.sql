76e4d1bc4891eeaa8d955e75338be31b4563371b
et.touil <et.touil505@gmail.com>
2024-03-14 19:22:37 +0000

    a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -2,4 +2,4 @@
 
 -- display both the score and the name (in this order) ordered by score (top first)
 
-SELECT score, name FROM second_table ORDER BY score;
+SELECT score, name FROM second_table ORDER BY score DESC;
