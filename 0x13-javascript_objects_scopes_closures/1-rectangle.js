cc49cf8c43c6faff581ae4aabcf0d827144e08ce
et.touil <et.touil505@gmail.com>
2024-04-09 23:16:23 +0000

    a class Rectangle that defines a rectangle:

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,10 +2,20 @@
 
 class Rectangle {
   constructor(w, h) {
-    this.width = w;
-    this.height = h;
+    if (w <= 0 || h <= 0) {
+      // If either width or height is invalid, set both to 0
+      this.width = 0;
+      this.height = 0;
+    } else if (h === undefined) {
+      // If only one argument is provided, set both width and height to the same value
+      this.width = w;
+      this.height = w;
+    } else {
+      // Otherwise, initialize width and height with provided values
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 
 module.exports = Rectangle;
-
