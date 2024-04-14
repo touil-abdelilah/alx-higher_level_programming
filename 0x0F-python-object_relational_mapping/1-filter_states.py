9efe7276d51767ea385302b446630343f5f9257b
et.touil <et.touil505@gmail.com>
2024-04-14 17:23:19 +0100

    a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Script to list all states with a name starting with N from the database hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+def filter_states(username, password, database):
+    """
+    Function to list all states with a name starting with N from the database hbtn_0e_0_usa
+    """
+    # Connect to MySQL server running on localhost at port 3306
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Execute the query to select states with name starting with 'N'
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Print the results
+    for row in results:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    # Get MySQL username, password, and database name from command-line arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    filter_states(username, password, database)
+
