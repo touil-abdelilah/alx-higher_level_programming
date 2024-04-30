0136f01334ce8fc4a3be1d7c2b85e85b0f28756c
et.touil <et.touil505@gmail.com>
2024-05-01 00:48:31 +0100

    a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-# Send a request to the URL using curl and store the response body in a variable
-response=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}')
-
-# Display the size of the body of the response
-echo "$response"
+# cURL b0dy siz3
+curl -sw '%{size_download}\n' -o /dev/null "$1"
