5c0fdbc312c34f5a0ca47a03bd17771c2bd80bc4
et.touil <et.touil505@gmail.com>
2024-07-18 18:19:21 +0100

    a script that reads and prints the content of a file. The first argument is the file path The content of the file must be read in utf-8 If an error occurred during the reading, print the error object

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
+
+// Import the 'fs' module to handle file system operations
 const fs = require('fs');
-const file = process.argv[2];
+
+// Get the file path from the command line arguments
+const filePath = process.argv[2];
+
+// Try to read the file synchronously
 try {
-  const data = fs.readFileSync(file, 'utf8');
+  // Read the file contents as a UTF-8 encoded string
+  const data = fs.readFileSync(filePath, 'utf8');
+
+  // Print the file contents to the console
   console.log(data);
 } catch (err) {
+  // If an error occurs (e.g., file not found), print the error object
   console.error(err);
 }
