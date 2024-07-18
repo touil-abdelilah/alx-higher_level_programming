3e618a51775b7442e86fcc5e81bdba94d9809f29
et.touil <et.touil505@gmail.com>
2024-07-18 18:29:12 +0100

    a script that reads and prints the content of a file. The first argument is the file path The content of the file must be read in utf-8 If an error occurred during the reading, print the error object

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 
+// Import the 'fs' module to handle file system operations
 const fs = require('fs');
 
+// Use the 'readFile' method to read the file asynchronously
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  // If an error occurs, print the error object to the console
   if (err) {
     console.log(err);
   } else {
+    // If no error occurs, print the file contents to the console
     process.stdout.write(data);
   }
 });
